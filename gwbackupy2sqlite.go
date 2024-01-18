de1ffd351b96c060eea11bbfc7e69a5a9f185987
Udi Finkelstein <github@udifink.com>
2024-01-18 22:16:20 +0200

    snapshot. this is only for debugging the date module. lots of non functional code

--- gwbackupy2sqlite.go
+++ gwbackupy2sqlite.go
@@ -12,6 +12,7 @@ import (
 	"net/mail"
 	"os"
 	"path/filepath"
+	"regexp"
 	"runtime"
 	"strconv"
 	"strings"
@@ -20,6 +21,7 @@ import (
 
 	//_ "github.com/mattn/go-sqlite3"
 	_ "github.com/glebarez/go-sqlite"
+	"github.com/udif/yaccDate/yaccDate"
 	"golang.org/x/text/encoding"
 	"golang.org/x/text/encoding/charmap"
 	"golang.org/x/text/encoding/simplifiedchinese"
@@ -65,7 +67,7 @@ func Max(x, y int) int {
 func CharsetReader(charset string, input io.Reader) (io.Reader, error) {
 	var dec *encoding.Decoder
 	switch strings.ToLower(charset) {
-	case "iso-8859-8", "iso-8859-8-i":
+	case "iso-8859-8", "iso-8859-8-i", "unknown":
 		// Replace with the actual ISO-8859-8 decoder
 		dec = charmap.ISO8859_8.NewDecoder()
 	case "windows-1255":
@@ -101,6 +103,8 @@ func convertRawToUTF8(s string) string {
 	}
 	return s
 }
+
+var logger *log.Logger
 
 var dateLayout1 = []string{
 	"Mon, 2 Jan 2006 15:4:5 -0700",
@@ -164,17 +168,55 @@ func parseDateFlexible(dateStr string) time.Time {
 	return t
 }
 
+// var tzones = make(map[string]bool)
+var tzones sync.Map
+
+func canonizeDateFormat(input string) string {
+	input2 := strings.ToLower(input)
+	weekdays_months := map[string]string{
+		"monday": "sunday", "tuesday": "sunday", "wednesday": "sunday", "thursday": "sunday", "friday": "sunday", "saturday": "sunday",
+		"mon": "sun", "tue": "sun", "wed": "sun", "thu": "sun", "fri": "sun", "sat": "sun",
+		"february": "january", "march": "january", "april": "january", "may": "january", "june": "january", "july": "january", "august": "january", "september": "january", "october": "january", "november": "january", "december": "january",
+		"feb": "jan", "mar": "jan", "apr": "jan", "jun": "jan", "jul": "jan", "aug": "jan", "sep": "jan", "oct": "jan", "nov": "jan", "dec": "jan",
+	}
+
+	for k, v := range weekdays_months {
+		input2 = strings.ReplaceAll(input2, k, v)
+	}
+
+	re := regexp.MustCompile(`\b[a-z]{3}\b`)
+	matches := re.FindAllString(input2, -1)
+	for _, match := range matches {
+		m := strings.ToLower(match)
+		if m != "jan" && m != "sun" && m != "gmt" && m != "utc" {
+			_, exists := tzones.Load(match)
+			if !exists {
+				tzones.Store(match, true)
+			}
+			input2 = strings.ReplaceAll(input2, match, "XXX")
+		}
+	}
+
+	re = regexp.MustCompile(`\d`)
+	input2 = re.ReplaceAllString(input2, "0")
+
+	return "## " + input + " => " + input2
+}
+
 func handleMail(goroutineNum int, filename string, resultCh chan<- string) {
 	var gzName, jsonName string
-	dir, file := filepath.Split(filename)
+	_, file := filepath.Split(filename)
+	dir := filepath.Dir(filename)
 	id, err := strconv.ParseInt(file[0:16], 16, 64)
 	if err != nil {
+		fmt.Println("Filename:", filename)
 		panic(err)
 	}
 	//fmt.Println(id)
 	ext := filepath.Ext(filename)
 	fileMu.Lock() // release lock ASAP
 	val, ok := fileMap[id]
+	dir2 := filepath.Dir(val)
 	if !ok {
 		// We got json, but if no map hit, we only got 1 file yet
 		fileMap[id] = filename // save 1st filename
@@ -189,22 +231,22 @@ func handleMail(goroutineNum int, filename string, resultCh chan<- string) {
 		jsonName = filename
 		gzName = val
 		if ext2 != ".gz" {
-			log.Fatal("ext == json but ext2 != gz")
+			fmt.Println(filename, val)
+			logger.Fatal("ext == json but ext2 != gz")
 		}
 	} else {
 		jsonName = val
 		gzName = filename
 		if ext != ".gz" {
-			log.Fatal("ext != gz and ext != json", ext)
+			logger.Fatal("ext != gz and ext != json", ext)
 		}
 		if ext2 != ".json" {
-			log.Fatal("ext == gz but ext2 != json")
+			logger.Fatal("ext == gz but ext2 != json")
 		}
 	}
 	//fmt.Println("jsonName:", jsonName, "gzName:", gzName)
-	dir2, _ := filepath.Split(filename)
 	if dir != dir2 {
-		log.Fatal("dir != dir2")
+		logger.Fatal("dir != dir2")
 	}
 	// optional sanity checks:
 	// both filename has the same id
@@ -213,6 +255,8 @@ func handleMail(goroutineNum int, filename string, resultCh chan<- string) {
 	//dir3, day := filepath.Split(filepath.Clean(dir))
 	//_, year := filepath.Split(filepath.Clean(dir3))
 	//fmt.Println("gzip: year:", year, "day:", day, "id:", id)
+	month_day := filepath.Base(dir2)
+	year := filepath.Base(filepath.Dir(dir2))
 
 	var dec = new(mime.WordDecoder)
 	dec.CharsetReader = CharsetReader
@@ -222,7 +266,7 @@ func handleMail(goroutineNum int, filename string, resultCh chan<- string) {
 	// ***************************
 	byteValue, err := os.ReadFile(jsonName)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 	var email Emails
 	json.Unmarshal([]byte(byteValue), &email)
@@ -233,37 +277,76 @@ func handleMail(goroutineNum int, filename string, resultCh chan<- string) {
 	// Handle gz file (email payload)
 	fhg, err := os.Open(gzName)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 	defer fhg.Close()
 
 	gz, err := gzip.NewReader(fhg)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 	defer gz.Close()
 
 	msg, err := mail.ReadMessage(gz)
 	if err != nil {
-		log.Fatal(err)
+		if len(filename) > 0 {
+			logger.Println("Filename:", filename)
+		} else {
+			logger.Println("nil Filename!")
+		}
+		logger.Println(err)
+		return
 	}
+	encodedDate := msg.Header.Get("Date")
+	fmt.Println(canonizeDateFormat(encodedDate))
+	return
 
-	encodedSubject := msg.Header.Get("Subject")
-	decodedSubject, err := dec.DecodeHeader(encodedSubject)
-	if &decodedSubject == &encodedSubject {
-		// If both vars point to the same strings, it means no new buffer was allocated,
-		// and no quote-printable string ('=?') was found in encodedSubject
-		decodedSubject = convertRawToUTF8(encodedSubject)
+	var chat bool = false
+	var decodedSubject string
+	for _, v := range email.LabelIds {
+		if v == "CHAT" {
+			chat = true
+		}
+	}
+	if !chat {
+		encodedSubject := msg.Header.Get("Subject")
+		decodedSubject, err := dec.DecodeHeader(encodedSubject)
+		if &decodedSubject == &encodedSubject {
+			// If both vars point to the same strings, it means no new buffer was allocated,
+			// and no quote-printable string ('=?') was found in encodedSubject
+			decodedSubject = convertRawToUTF8(encodedSubject)
+		}
+		//fmt.Println(msg.Header.Get("Date"))
+		encodedDate := msg.Header.Get("Date")
+		t, err := yaccDate.FlexDateToTime(encodedDate) // used to be time.RFC1123Z
+		idate := int64(email.InternalDate) / 1000
+		if err != nil {
+			fmt.Printf("%s/%s/%016x: Date='%s' Error:%s\n", year, month_day, id, encodedDate, err)
+			email.Date_e = idate
+			os.Exit(1)
+		} else {
+			decodedDate, _ := dec.DecodeHeader(encodedDate)
+			if &decodedDate == &encodedDate {
+				// If both vars point to the same strings, it means no new buffer was allocated,
+				// and no quote-printable string ('=?') was found in encodedSubject
+				decodedDate = convertRawToUTF8(encodedDate)
+			}
+			email.Date_e = t.Unix()
+			if email.Date_e != idate {
+				fmt.Println(t)
+				fmt.Printf("%s/%s/%016x: Date='%s' vs '%s', %d != %d\n", year, month_day, id, decodedDate, time.Unix(idate, 0), email.Date_e, idate)
+				os.Exit(1)
+			} else {
+				fmt.Printf("%s/%s/%016x: Date='%s'\n", year, month_day, id, encodedDate)
+			}
+		}
+	} else {
+		decodedSubject = "Chat with " // + TODO
+		// gmail derives it from datetime of last chat message, translated to your local TZ
+		// dates in chat are encoded as GMT+0
+		email.Date_e = int64(email.InternalDate) / 1000
 	}
 	email.Subject_e = decodedSubject
-	//fmt.Println(msg.Header.Get("Date"))
-	t := parseDateFlexible(msg.Header.Get("Date")) // used to be time.RFC1123Z
-	//t, err := datetime.Parse(msg.Header.Get("Date"), time.Local)
-	//if err != nil {
-	//	fmt.Println("Error:", err)
-	//	return
-	//}
-	email.Date_e = t.Unix()
 	//fmt.Println(email)
 
 	modifiedSubject := fmt.Sprintf("%s: %s : %d", "" /*filename*/, decodedSubject, goroutineNum)
@@ -295,7 +378,7 @@ func upgradeDb(db *sql.DB, dbv *SemVer) {
 func openDatabase(db_name string) (db *sql.DB, err error) {
 	db, err = sql.Open("sqlite", db_name)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 
 	var table string
@@ -307,15 +390,15 @@ func openDatabase(db_name string) (db *sql.DB, err error) {
 			// read SQL commands from file
 			sqlCommands, err := os.ReadFile("tables.sql")
 			if err != nil {
-				log.Fatal(err)
+				logger.Fatal(err)
 			}
 			// execute SQL commands
 			_, err = db.Exec(string(sqlCommands))
 			if err != nil {
-				log.Fatal(err)
+				logger.Fatal(err)
 			}
 		} else {
-			log.Fatal(err)
+			logger.Fatal(err)
 		}
 	} else {
 		cv := SemVer{major: 1, minor: 0, patch: 0}
@@ -332,20 +415,60 @@ func openDatabase(db_name string) (db *sql.DB, err error) {
 func main() {
 	dir := flag.String("dir", "", "directory path")
 	db_name := flag.String("db", "", "database name")
+	logname := flag.String("log", "", "logfile name")
 	numProcs := flag.Int("procs", Max(1, runtime.NumCPU()-2), "number of parallel processes")
 	flag.Parse()
 	if *dir == "" || *db_name == "" {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
-
+	if *logname != "" {
+		file, err := os.OpenFile(*logname, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		logger = log.New(file, "", log.LstdFlags)
+	}
 	var files []string
+	/*
+		years, err := os.ReadDir(*dir)
+		if err != nil {
+			logger.Fatal(err)
+		}
+
+		for _, year := range years {
+			if year.IsDir() {
+				match, _ := regexp.MatchString(`^\d{4}$`, s)
+				if !match {
+					continue
+				}
+				year_dir := filepath.Join(*dir, year.Name())
+				if
+				month_days, err := os.ReadDir(year_dir)
+				if err != nil {
+					logger.Fatal(err)
+				}
+				for _, month_day := range month_days {
+					month_day_dir := filepath.Join(year_dir, month_day.Name())
+					if month_day.IsDir() {
+						match, _ = regexp.MatchString(`^\d{2}-\d{2}$`, s)
+						fmt.Println(month_day_dir)
+					}
+				}
+			}
+		}
+		os.Exit(0)
+	*/
+	re := regexp.MustCompile(`\d{4}/\d{2}-\d{2}/[0-9A-Fa-f]{16}[^/]*$`)
 	err := filepath.Walk(*dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 		if !info.IsDir() {
-			files = append(files, path)
+			path2 := filepath.ToSlash(path)
+			if match := re.MatchString(path2); match {
+				files = append(files, path)
+			}
 		}
 		return nil
 	})
@@ -373,6 +496,7 @@ func main() {
 
 	// Send each filename to a goroutine.
 	for _, filename := range files {
+		//fmt.Println(filename)
 		if strings.HasSuffix(filename, ".gz") {
 			fileCh <- filename
 		}
@@ -381,6 +505,7 @@ func main() {
 		}
 	}
 
+	fmt.Println(tzones)
 	close(fileCh)
 	wg.Wait()
 	close(resultCh)
